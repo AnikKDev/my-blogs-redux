4eda3b0f6a7c3a771bed54830a8e31f43107bb47
anikdev2016 <anikdev2016@gmail.com>
2023-02-04 14:04:22 +0600

    created blog posting form

--- src/pages/Blogs/WriteBlog/WriteBlog.jsx
+++ src/pages/Blogs/WriteBlog/WriteBlog.jsx
@@ -1,7 +1,55 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Select from "react-select";
+// import { colourOptions } from "../data";
 const WriteBlog = () => {
-  return <div className="">this is write blog page</div>;
+  const colourOptions = [
+    { value: "programming", label: "programming" },
+    { value: "javascript", label: "javascript" },
+    { value: "google", label: "google" },
+  ];
+  const [selected, setSelected] = useState(null);
+  const handleChange = (selectedOption) => {
+    setSelected(selectedOption.value);
+    console.log(`Option selected:`, selectedOption);
+  };
+  return (
+    <div className="my-16 border-3 shadow-orange-100 shadow-lg p-8 rounded-lg">
+      {/* blog area */}
+      <div className="my-6">
+        <label htmlFor="blogTitle">Title</label>
+        <input
+          type="text"
+          id="blogTitle"
+          className="input input-bordered focus-visible:outline-gray-500 transition-all w-full"
+        />
+      </div>
+      <div>
+        <label htmlFor="writeBlog">Write Here</label>
+        <textarea
+          id="writeBlog"
+          name="writeBlog"
+          placeholder="Write a blog"
+          className="textarea visible:bg-orange-500 transition-all focus-visible:outline-gray-500 border-3 textarea-bordered textarea-lg w-full h-64"
+        ></textarea>
+      </div>
+      <div>
+        <label htmlFor="blogTag">Tags</label>
+
+        <Select
+          // defaultValue={[colourOptions[2], colourOptions[3]]}
+          isMulti
+          name="colors"
+          options={colourOptions}
+          className="basic-multi-select focus-visible:outline-gray-500"
+          classNamePrefix="select"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex items-center justify-center my-10">
+        <button className="btn btn-success text-white">Create Blog</button>
+      </div>
+    </div>
+  );
 };
 
 export default WriteBlog;
--- src/pages/MainPage/Main.jsx
+++ src/pages/MainPage/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <div>
       <Navbar />
-      <div className="border-2 border-red-600 pt-16 max-w-7xl w-10/12 mx-auto">
+      <div className="pt-16 max-w-7xl w-10/12 mx-auto">
         <Outlet />
       </div>
     </div>
